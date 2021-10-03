967b5593b1e9d3d960596e3678c9191cd2f44280
wellpinho <wellpinho@outlook.com>
2021-10-02 21:27:20 -0300

    test: ensure SignupController returns an error if no email is provider

--- src/presentation/controller/signupController.spec.ts
+++ src/presentation/controller/signupController.spec.ts
@@ -14,4 +14,18 @@ describe('signup controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('should be return 400 if no email is provider', () => {
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
